946dea71603914ebe2bf7b47539e4d5373e8a2d4
mchl02 <matthewchlee02@gmail.com>
2018-12-06 22:31:47 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -81,3 +81,9 @@ function Person() {
   
   var p = new Person();
   console.log(p)
+
+var x = [0,1,2,3,4,5,6,7,8,9]
+var a = [x, x, x, x, x];
+
+for (var i = 0, j = 9; i <= j; i++, j--)
+  console.log('a[' + i + '][' + j + ']= ' + a[i][j]);
