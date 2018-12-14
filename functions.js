f893f28f367455e31d7dbf0cd1c84cfdf8bb0098
mchl02 <matthewchlee02@gmail.com>
2018-12-13 23:21:52 -0500

    Update functions.js

--- functions.js
+++ functions.js
@@ -105,3 +105,17 @@ const hello = 'Hello, World!';
 const helloLength = hello.length;
 hello[0] = 'L'; // This has no effect, because strings are immutable
 hello[0]; // This returns "H"
+
+const names = ["George","Michael","Dylan"];
+console.log(names.sort().join(', '));
+const msPerDay= 24*60*60*1000;
+const datesMenu = new Date(msPerDay * (44 * 365 + 11 + 197));
+
+const options = { year: '2-digit', month: '2-digit', day: '2-digit',
+                hour: '2-digit', minute: '2-digit', timeZoneName: 'short' };
+const americanDateTime = new Intl.DateTimeFormat('en-US', options).format;
+ 
+console.log(americanDateTime(datesMenu));
+
+console.log("Below will start the regular expressions:")
+
