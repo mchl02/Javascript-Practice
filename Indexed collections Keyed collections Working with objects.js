c59ba85805fead836106d4332b893b09d10eb56c
mchl02 <matthewchlee02@gmail.com>
2019-01-10 23:37:47 -0500

    Update Indexed collections Keyed collections Working with objects.js

--- Indexed collections Keyed collections Working with objects.js
+++ Indexed collections Keyed collections Working with objects.js
@@ -45,5 +45,22 @@ for (i = 0; i < 4; i++) {
 //Start Keyed Collections
 
 //Sets
+var sayings = new Map();
+sayings.set('dog', 'woof');
+sayings.set('cat', 'meow');
+sayings.set('elephant', 'toot');
+sayings.size; // 3
+sayings.get('fox'); // undefined
+sayings.has('bird'); // false
+sayings.delete('dog');
+sayings.has('dog'); // false
 
+for (var [key, value] of sayings) {
+  console.log(key + ' goes ' + value);
+}
+// "cat goes meow"
+// "elephant goes toot"
+
+sayings.clear();
+sayings.size; // 0
 
