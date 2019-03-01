f09d469cdd381235b45c750f7be32fa1722e321e
mchl02 <matthewchlee02@gmail.com>
2019-03-01 15:45:13 -0500

    Update Indexed collections Keyed collections Working with objects.js

--- Indexed collections Keyed collections Working with objects.js
+++ Indexed collections Keyed collections Working with objects.js
@@ -247,3 +247,28 @@ var vpgscar = new Car('Mazda', 'Miata', 1990);
 
 
 
+
+function Person(name, age, sex) {
+  this.name = name;
+  this.age = age;
+  this.sex = sex;
+}
+
+var rand = new Person('Rand McKinnon', 33, 'M');
+var ken = new Person('Ken Jones', 39, 'M');
+
+function Car(make, model, year, owner) {
+  this.make = make;
+  this.model = model;
+  this.year = year;
+  this.owner = owner;
+}
+
+var car1 = new Car('Eagle', 'Talon TSi', 1993, rand);
+var car2 = new Car('Nissan', '300ZX', 1992, ken);
+
+car2.owner.name
+
+car1.color = 'black';
+
+//object.create method below 
